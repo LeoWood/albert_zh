f00d801ab1f99bd8dc8f732b82876b4a7f1a421a
LeoWood <463861936@qq.com>
2019-12-10 20:59:03 +0800

    add Exp1

--- Exp1.bat
+++ Exp1.bat
@@ -1,15 +1,15 @@
 python run_classifier.py^
  --data_dir data/data16000^
- --bert_config_file models/albert_large_zh/albert_config_large.json^
+ --bert_config_file models/albert_large_zh/config.json^
  --task_name cla^
  --vocab_file models/albert_large_zh/vocab.txt^
  --output_dir outputs/Exp1^
  --init_checkpoint models/albert_large_zh^
- --max_seq_length 128^
+ --max_seq_length 400^
  --do_train True^
  --do_eval True^
  --do_predict False^
- --train_batch_size 32^
+ --train_batch_size 10^
  --learning_rate 5e-5^
  --num_train_epochs 3.0
   
